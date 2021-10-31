8c92171cec819250e4bf4db7101bb98577f91a71
Ayoub Kaanich <ayoub.kaanich@technica-engineering.de>
2021-10-31 07:53:56 +0100

    Simplify zstd cmake logic (#15)

--- cmake/zstd.cmake
+++ cmake/zstd.cmake
@@ -1,61 +1,58 @@
 cmake_minimum_required(VERSION 3.11)
 
+add_library(light_zstd INTERFACE)
+
+function(zstd_libraries SHARED_LIB STATIC_LIB)
+    if(BUILD_SHARED_LIBS)
+        target_link_libraries(light_zstd INTERFACE ${SHARED_LIB})
+    else()
+        target_link_libraries(light_zstd INTERFACE ${STATIC_LIB})
+        # this pthread linking is required for the static linking of certain zstd
+        # versions. See: https://github.com/facebook/zstd/pull/2097
+        find_package(Threads REQUIRED)
+        target_link_libraries(light_zstd INTERFACE ${CMAKE_THREAD_LIBS_INIT})
+    endif()
+endfunction()
+
 # Try to find zstd in the cmake search path
 find_package(zstd QUIET)
 if(zstd_FOUND)
-  # zstd was already found, so we don't resolve our dependency by ourself Check
-  # if dependency was resolved by conan (find_package generator)
-  if(CONAN_ZSTD_ROOT)
-    set(light_zstd_dependency_name zstd::zstd)
-  else()
-    if(BUILD_SHARED_LIBS)
-      set(light_zstd_dependency_name zstd::libzstd_shared)
+    if(TARGET zstd::zstd)
+        # dependency was resolved by conan (find_package generator)
+        target_link_libraries(light_zstd INTERFACE zstd::zstd)
     else()
-      set(light_zstd_dependency_name zstd::libzstd_static)
+        zstd_libraries(zstd::libzstd_shared zstd::libzstd_static)
     endif()
-  endif()
 else()
-  # try finding the library on the system with the traditional method (no cmake
-  # package on system)
-  find_library(libzstd_static NAMES libzstd.a)
-  find_library(libzstd_shared NAMES libzstd.so)
-  if(libzstd_static AND libzstd_shared)
-    if(BUILD_SHARED_LIBS)
-      set(light_zstd_dependency_name ${libzstd_shared})
-    else()
-      set(light_zstd_dependency_name ${libzstd_static})
+    # zstd was not found, so we do not resolve our dependency by ourself
+    # try finding the library on the system with the traditional method 
+    # (no cmake package on system)
+    find_path(ZSTD_INCLUDE_DIRS zstd.h)
+    find_library(ZSTD_STATIC_LIB NAMES libzstd.a)
+    find_library(ZSTD_SHARED_LIB NAMES libzstd.so)
+
+    if(ZSTD_INCLUDE_DIRS AND ZSTD_STATIC_LIB AND ZSTD_SHARED_LIB)
+        zstd_libraries("${ZSTD_SHARED_LIB}" "${ZSTD_STATIC_LIB}")
+        target_include_directories(light_zstd INTERFACE "${ZSTD_INCLUDE_DIRS}")
+        set(zstd_FOUND ON)
     endif()
-  else()
+endif()
+
+if(NOT zstd_FOUND)
     # compile library if not found
     include(FetchContent)
 
     FetchContent_Declare(
-      zstd
-      GIT_REPOSITORY https://github.com/facebook/zstd.git
-      GIT_TAG v1.4.5)
+        zstd
+        GIT_REPOSITORY https://github.com/facebook/zstd.git
+        GIT_TAG v1.5.0)
+
     FetchContent_GetProperties(zstd POPULATED ZSTD_POPULATED)
+
     if(NOT ZSTD_POPULATED)
-      FetchContent_Populate(zstd)
-      add_subdirectory(${zstd_SOURCE_DIR}/build/cmake ${zstd_BINARY_DIR} EXCLUDE_FROM_ALL)
+        FetchContent_Populate(zstd)
+        add_subdirectory("${zstd_SOURCE_DIR}/build/cmake" "${zstd_BINARY_DIR}" EXCLUDE_FROM_ALL)
     endif()
-
-    include_directories(${zstd_SOURCE_DIR}/lib)
-
-    if(BUILD_SHARED_LIBS)
-      set(light_zstd_dependency_name libzstd_shared)
-    else()
-      set(light_zstd_dependency_name libzstd_static)
-    endif()
-  endif()
-endif()
-
-add_definitions(-DUSE_ZSTD)
-target_link_libraries(light_pcapng ${light_zstd_dependency_name})
-
-
-# this pthread linking is required for the static linking of certain zstd
-# version. See: https://github.com/facebook/zstd/pull/2097
-if(NOT BUILD_SHARED_LIBS)
-  find_package(Threads REQUIRED)
-  target_link_libraries(light_pcapng ${CMAKE_THREAD_LIBS_INIT})
+    zstd_libraries(libzstd_shared libzstd_static)
+    target_include_directories(light_zstd INTERFACE "${zstd_SOURCE_DIR}/lib")
 endif()
